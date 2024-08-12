5c3e0755cadcd5005b2022d0afdd94e971be6753
eshaalal <eshalal888@gmail.com>
2024-08-12 15:52:24 +0530

    changes

--- lect1.js
+++ lect1.js
@@ -1,7 +1,64 @@
-function sayHello(){
-    for(var i=0;i<5;i++){
-        console.log(i);
-    }
-    console.log(i);
-}
-sayHello();
+//1
+
+// function sayHello(){
+//     for(var i=0;i<5;i++){
+//         console.log(i);
+//     }
+//     console.log(i);
+// }
+// sayHello();
+
+//2
+
+// class Student{
+//     constructor(name,age){
+//         this.n=name;
+//         this.a=age;
+//     }
+//     stu(){
+//         console.log("The name of the student is:", this.n);
+//         console.log("The age of the student is:",this.a); 
+//     }
+// }
+// var stuObj= new Student('esha ',20);
+// stuObj.stu()
+
+//3
+
+// class Car{
+//     constructor(name){
+//         this.brand=name;
+//     }
+//     present(){
+//         return 'I have a '+ this.brand;
+//     }
+// }
+// class Model extends Car{
+//     constructor(name,mod){
+//         super(name);
+//         this.model=mod;
+//     }
+//     show(){
+//         console.log(this.present()+', it is a '+ this.model)
+//     }
+// }
+// const myCar= new Model("FORD","Mustang");
+// myCar.show();
+
+// function multiply(a, b){
+//     return a*b;
+// } 
+
+
+//4
+
+// value=(a,b)=>a*b;
+// console.log(value(3,5)); 
+
+//5
+
+
+
+
+
+
