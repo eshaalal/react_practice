6510352d1da15967348e5b03dfb9cb27f5d3bb31
eshaalal <eshalal888@gmail.com>
2024-08-26 14:25:37 +0530

    changes

---
+++ lect3.js
@@ -0,0 +1,6 @@
+var numbers=[4,6,7,9,2,1];
+var roots=numbers.map(Math.sqrt);
+console.log("roots are: "+ roots);
+
+const value=(a,b)=>a*b;
+console.log(value(3,5)); 
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,3 +1,1 @@
-function multiply(a, b){
-//     return a*b;
-// }
+console.log(value(3,5)); 
