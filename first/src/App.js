989475d96ae0f8f1cbe8b8516e04924478dcf9a6
eshaalal <eshalal888@gmail.com>
2024-09-02 15:32:40 +0530

    changes

--- first/src/App.js
+++ first/src/App.js
@@ -1,8 +1,22 @@
+// App.js
+
+import React from 'react';
+import Navbar from './Navbar';
+
+function Car(props) {
+  return <h2>I am a {props.brand} car</h2>;
+}
 
 function App() {
   return (
-    <p>Lovely Professional University</p>
-  )
+    <div className="App">
+      <header className="App-header">
+        <Navbar/>
+        <Car brand="Tesla" />
+        <Car brand="Toyota" />
+      </header>
+    </div>
+  );
 }
 
 export default App;
---
+++ first/src/Navbar.js
@@ -0,0 +1,18 @@
+// Navbar.js
+
+import React from 'react';
+
+function Navbar() {
+  return (
+    <nav>
+      
+      <a href="/">Home</a>
+      <a href="/about">About</a>
+      <a href="/services">Services</a>
+      <a href="/contact">Contact</a>
+
+    </nav>
+  );
+}
+
+export default Navbar;
